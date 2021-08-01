6f21f7675358e87d80bf2794e9023b52897c3ed0
chavicoski <alvaro.lopez.chilet@gmail.com>
2021-08-01 20:24:32 +0200

    Added magic remap for find and replace

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -24,3 +24,8 @@ vim.api.nvim_set_keymap('v', 'J', "<Cmd>m '>+1<CR>gv=gv", { noremap = true })
 vim.api.nvim_set_keymap('v', 'K', "<Cmd>m '<-2<CR>gv=gv", { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>j', "<Cmd>m .+1<CR>==", { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>k', "<Cmd>m .-2<CR>==", { noremap = true })
+
+-- Better find-next and replace
+-- How to: Go to the word to change, press 'cn' or 'cN', type the new word and press '.'
+vim.api.nvim_set_keymap('n', 'cn', '*``cgn', { noremap = true })
+vim.api.nvim_set_keymap('n', 'cN', '*``cgN', { noremap = true })
