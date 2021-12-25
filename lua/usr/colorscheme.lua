3f8be78d1f437d3be73f1aedeb68297667a00ed5
chavicoski <alvaro.lopez.chilet@gmail.com>
2021-12-25 20:37:23 +0100

    Updated colorscheme config

--- lua/usr/colorscheme.lua
+++ lua/usr/colorscheme.lua
@@ -1,6 +1,16 @@
 -- Set the colorscheme configuration
-vim.o.background = 'dark'                                   -- 'dark' or 'light' mode (for gruvbox-material plugin)
-vim.api.nvim_set_var('gruvbox_material_background', 'hard') -- 'hard', 'medium', 'soft'
-vim.api.nvim_set_var('gruvbox_material_enable_bold', 1)     -- Functions in bold (like original gruvbox)
-vim.api.nvim_set_var('gruvbox_material_enable_italic', 1)   -- The font must support italic
-vim.cmd([[colorscheme gruvbox-material]])
+vim.o.background = 'dark'                                    -- 'dark' or 'light' mode (for gruvbox-material plugin)
+vim.api.nvim_set_var('gruvbox_material_palette', 'mix')      -- 'original', 'mix', 'material'
+vim.api.nvim_set_var('gruvbox_material_background', 'hard')  -- 'hard', 'medium', 'soft'
+vim.api.nvim_set_var('gruvbox_material_enable_bold', 1)      -- Functions in bold (like original gruvbox)
+vim.api.nvim_set_var('gruvbox_material_enable_italic', 1)    -- The font must support italic
+
+-- Choose the colorscheme
+local colorscheme = "gruvbox-material"
+
+-- Set colorscheme with a protected call to show a prettier error message
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  vim.notify("Error: colorscheme " .. colorscheme .. " not found!")
+  return
+end
