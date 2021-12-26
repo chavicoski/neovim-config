49cac226467bc8785a45965de7a0f2425e9282e4
chavicoski <alvaro.lopez.chilet@gmail.com>
2021-12-26 10:48:56 +0100

    Fixed typo

--- lua/usr/plugin_config/project.lua
+++ lua/usr/plugin_config/project.lua
@@ -1,7 +1,7 @@
 -- Protected call to generate prettier errors
-local project_nvim_status_ok, project_nvim = pcall(require, "project_nvim-tree")
+local project_nvim_status_ok, project_nvim = pcall(require, "project_nvim")
 if not project_nvim_status_ok then
-  vim.notify("[Error]: project_nvim-tree not found! (in project.lua)")
+  vim.notify("[Error]: project_nvim not found! (in project.lua)")
   return
 end
 
