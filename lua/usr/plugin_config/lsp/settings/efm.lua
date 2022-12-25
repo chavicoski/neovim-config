4454315bff71da7719b588115fdf191875e220a0
chavicoski <alvaro.lopez.chilet@gmail.com>
2022-12-25 22:36:57 +0100

    Added code formatting with Prettier

--- lua/usr/plugin_config/lsp/settings/efm.lua
+++ lua/usr/plugin_config/lsp/settings/efm.lua
@@ -6,6 +6,21 @@ return {
             python = {
                 { formatCommand = "black --quiet -", formatStdin = true },
                 { formatCommand = "isort --quiet -", formatStdin = true }
+            },
+            javascript = {
+                { formatCommand = "prettier ${INPUT}" },
+            },
+            typescript = {
+                { formatCommand = "prettier ${INPUT}" },
+            },
+            svelte = {
+                { formatCommand = "prettier ${INPUT}" },
+            },
+            html = {
+                { formatCommand = "prettier ${INPUT}" },
+            },
+            json = {
+                { formatCommand = "prettier ${INPUT}" },
             }
         }
     }
