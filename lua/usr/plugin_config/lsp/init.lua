367e06225a597ceba9f8e84317de0344dab5352c
chavicoski <alvaro.lopez.chilet@gmail.com>
2024-01-16 15:26:06 +0100

    Improved LSP keymaps config

--- lua/usr/plugin_config/lsp/init.lua
+++ lua/usr/plugin_config/lsp/init.lua
@@ -1,8 +1,11 @@
 local status_ok, lspconfig = pcall(require, "lspconfig")
 if not status_ok then
-  vim.notify("[Error]: lspconfig not found! (in lsp/init.lua)")
-  return
+    vim.notify("[Error]: lspconfig not found! (in lsp/init.lua)")
+    return
 end
+
+-- Set up the keymaps for the LSP server actions
+require("usr.plugin_config.lsp.keymaps")
 
 -- Plugins to install and configure LSP servers
 require("mason").setup()
@@ -10,63 +13,12 @@ require("mason-lspconfig").setup()
 
 require("usr.plugin_config.lsp.handlers").setup()
 
-local function lsp_highlight_document(client)
-  -- Set autocommands conditional on server_capabilities
-  if client.server_capabilities.document_highlight then
-    vim.api.nvim_exec(
-      [[
-      augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
-    ]],
-      false
-    )
-  end
-end
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-local function lsp_keymaps(bufnr)
-  local opts = { noremap = true, silent = true }
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-
-  buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-  buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-  buf_set_keymap('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-  buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-  buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-  buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-  buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  buf_set_keymap("n", "<leader>d", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-  buf_set_keymap("n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
-  buf_set_keymap("n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
-  buf_set_keymap("n", "gl", '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({ border = "rounded" })<CR>', opts)
-  buf_set_keymap("n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
-  buf_set_keymap("n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-  buf_set_keymap("n", "<S-A-f>", "<cmd>lua vim.lsp.buf.format({ async = true })<CR>", opts)
-  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.format({ async = true })' ]]
-end
-
-local on_attach = function(client, bufnr)
-  lsp_keymaps(bufnr)
-  lsp_highlight_document(client)
-end
-
-local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-if not status_ok then
-  vim.notify("[Error]: cmp_nvim_lsp not found! (in handlers.lua)")
-  return
-end
-
-local capabilities = cmp_nvim_lsp.default_capabilities()
-
-
-require("mason-lspconfig").setup_handlers{
-    function (server_name) -- default handler
+require("mason-lspconfig").setup_handlers {
+    function(server_name)  -- default handler
         require("lspconfig")[server_name].setup {
             capabilities = capabilities,
-            on_attach = on_attach
         }
     end,
 
@@ -74,27 +26,26 @@ require("mason-lspconfig").setup_handlers{
     ["lua_ls"] = function()
         lspconfig["lua_ls"].setup {
             capabilities = capabilities,
-            on_attach = on_attach,
             settings = {
                 Lua = {
-                  runtime = {
-                    -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                    version = 'LuaJIT',
-                  },
-                  diagnostics = {
-                    -- Get the language server to recognize the `vim` global
-                    globals = {'vim'},
-                  },
-                  workspace = {
-                    -- Make the server aware of Neovim runtime files
-                    library = vim.api.nvim_get_runtime_file("", true),
-                  },
-                  -- Do not send telemetry data containing a randomized but unique identifier
-                  telemetry = {
-                    enable = false,
-                  },
+                    runtime = {
+                        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                        version = 'LuaJIT',
+                    },
+                    diagnostics = {
+                        -- Get the language server to recognize the `vim` global
+                        globals = { 'vim' },
+                    },
+                    workspace = {
+                        -- Make the server aware of Neovim runtime files
+                        library = vim.api.nvim_get_runtime_file("", true),
+                    },
+                    -- Do not send telemetry data containing a randomized but unique identifier
+                    telemetry = {
+                        enable = false,
+                    },
                 },
-              },
+            },
         }
     end,
     ["efm"] = function()
